e3aa95353b30064723118aafd5dd2b1e26bb2301
oginoshikibu <oginoshikibu@gmail.com>
2024-09-12 20:58:36 +0900

    諸々動いた

--- app/app.py
+++ app/app.py
@@ -1,8 +1,57 @@
-# 簡単なindexページの作成を行っています。
-from flask import Flask
+import os
+import dotenv
+from flask import Flask, request, abort
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
+
+dotenv.load_dotenv()
+
+CHANNEL_ACCESS_TOKEN=os.environ["CHANNEL_ACCESS_TOKEN"]
+CHANNEL_SECRET=os.environ["CHANNEL_SECRET"]
+
 
 app = Flask(__name__)
 
+line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(CHANNEL_SECRET)
+
 @app.route("/")
-def index():
-    return "index page"
+def hello_world():
+    return "Hello World!"
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=event.message.text))
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
+
